704659dc29d238f426f85e07d90435d1c5b99058
Nirajkaral <neerajcurl@gmail.com>
2026-03-05 16:34:46 +0530

    Day 2: Variables and data types in Python and Go

---
+++ Day 2/var.py
@@ -0,0 +1,16 @@
+# Day 2: Variables and Data Types
+
+name = "Niraj"
+age = 22
+height = 5.8
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of height:", type(height))
+print("Type of student:", type(is_student))
---
+++ Day 2/vari.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "Niraj"
+	var age int = 22
+	var height float64 = 5.8
+	var isStudent bool = true
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Height:", height)
+	fmt.Println("Student:", isStudent)
+}
