e85286a98d27816125cfcd747e215bc774b99e58
Nirajkaral <neerajcurl@gmail.com>
2026-03-06 15:01:22 +0530

    New-feature

--- Python/ifelse.py
+++ Python/ifelse.py
@@ -3,6 +3,7 @@
 number = int(input("Enter a number: "))
 
 if number % 2 == 0:
-    print("The number is Even")
+
+    print("The number is odd")
 else:
-    print("The number is Odd")
+    print("The number is even")
