dacd22576847959a362ccd46fb9ee05802592e74
Nirajkaral <neerajcurl@gmail.com>
2026-03-07 12:24:09 +0530

    New Task completed

---
+++ Python/import.py
@@ -0,0 +1,4 @@
+import calculator_module
+
+print("Addition:", calculator_module.add(5,3))
+print("Subtraction:", calculator_module.subtract(10,4))
---
+++ Python/todo_cli.py
@@ -0,0 +1,23 @@
+tasks = []
+
+while True:
+    print("\n1. Add Task")
+    print("2. View Tasks")
+    print("3. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        task = input("Enter new task: ")
+        tasks.append(task)
+
+    elif choice == "2":
+        print("\nYour Tasks:")
+        for t in tasks:
+            print("-", t)
+
+    elif choice == "3":
+        break
+
+    else:
+        print("Invalid option")
